21babc18bea735465dfdd7259b183aee852e69e7
yokkepp <yoseri430@gmail.com>
2023-08-26 13:28:42 +0900

    軽微な修正をしました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,52 +1,50 @@
 "use client";
-import Header from "@/components/Header/Header";
+import Header from "@/components/Header";
 import { Providers } from "./providers";
-import { useState, createContext, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import { db } from "./firebase";
-import {
-	collection,
-	deleteDoc,
-	doc,
-	getDocs,
-	updateDoc,
-} from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { LocalInformation, FirebaseInformation } from "./types";
-export const InformationsContextObject = createContext<
-	StateContextType | undefined
->(undefined);
 
-interface StateContextType {
-	informations: FirebaseInformation[];
-	setInformations: React.Dispatch<React.SetStateAction<never[]>>;
-}
+export const InformationsContext = createContext<
+	FirebaseInformation[] | undefined
+>(undefined);
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const [informations, setInformations] = useState([]);
+	/*------------------------------------------------------------
+共通処理はここに記載する。
+------------------------------------------------------------*/
+
+	// import React, { useState, createContext } from "react";
+
+	const [informations, setInformations]: any = useState<FirebaseInformation[]>(
+		[]
+	);
 
 	//レンダリング時にfirebaseからデータを読み込む。
 	useEffect(() => {
 		const infoData = collection(db, "informations");
 		getDocs(infoData).then((result) => {
-			const INITIAL_DATA: any = [];
+			const INITIAL_DATA: any[] = [];
 			result.forEach((doc) => {
 				INITIAL_DATA.push({ ...doc.data(), id: doc.id });
 			});
 			setInformations(INITIAL_DATA);
 		});
 	}, []);
+
 	return (
 		<html lang='ja'>
 			<body>
 				<Providers>
-					<InformationsContextObject.Provider
-						value={{ informations, setInformations }}>
+					<InformationsContext.Provider value={[informations, setInformations]}>
 						<Header />
 						{children}
-					</InformationsContextObject.Provider>
+					</InformationsContext.Provider>
 				</Providers>
 			</body>
 		</html>
--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,33 +1,18 @@
 "use client";
-import { InformationsContextObject } from "../layout";
-import { useState, useContext, SetStateAction } from "react";
-import { Modal } from "@/components/Modal/Modal";
+import { Box, Stack, Button, SimpleGrid } from "@chakra-ui/react";
+import { useState, useContext } from "react";
+import { Modal } from "@/components/Modal";
+import ActiveNote from "@/app/utils/Notes/ActiveNote";
+import NoteLists from "@/app/utils/Notes/NoteLists";
 import TasksParts from "@/components/TasksParts";
 import NotesParts from "@/components/NotesParts";
 import BoardsParts from "@/components/BoardsParts";
 import { BgMaskForInput } from "@/components/bgMaskForInput";
-import {
-	Text,
-	Box,
-	Stack,
-	Button,
-	Checkbox,
-	SimpleGrid,
-	Input,
-	Textarea,
-} from "@chakra-ui/react";
-import { db } from "../firebase";
-import {
-	collection,
-	deleteDoc,
-	doc,
-	getDocs,
-	updateDoc,
-} from "firebase/firestore";
-import NoteLists from "@/components/NoteLists";
-import ActiveNote from "@/components/ActiveNote";
 import { LocalInformation, FirebaseInformation } from "../types";
-import { INITIAL_EDITING, INITIAL_INFORMATION } from "../../config/initial";
+import { INITIAL_EDITING, INITIAL_INFORMATION } from "../consts/initial";
+import { db } from "../firebase";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { InformationsContext } from "../layout";
 
 export default function Notes() {
 	//新しいNoteを追加する時のモーダルの表示状態を管理します。
@@ -44,38 +29,18 @@ export default function Notes() {
 		useState<LocalInformation>(INITIAL_INFORMATION);
 
 	//全てのデータを管理します。
-	const { informations, setInformations } = useContext<any>(
-		InformationsContextObject
-	);
-
-	/**
-	 *モーダル内部の
-	 * @param e イベントです。
-	 */
-	const handleChangeInformation = (
-		e: { target: HTMLButtonElement },
-		name: string
-	) => {
-		setActiveInformation((prev) => ({ ...prev, [name]: e.target.value }));
-	};
+	const [informations, setInformations] = useContext<any>(InformationsContext);
 
 	/**
 	 *クリックされた要素だけを編集中のステータスに変更するための関数です。
 	 * @param e イベントです。
 	 */
-	const handleClickUpdateElement = async (e: { target: { id: string } }) => {
+	const handleClickUpdateElement = async (e: any) => {
 		//対象のidに設定されたプロパティ名を取得する。
 		setEditingElement(e.target.id);
 		//一度初期化し、クリックされた要素だけを編集中のステータスに変更する
 		const newEditing = { ...INITIAL_EDITING };
 		setIsEditing({ ...newEditing, [e.target.id]: true, all: true });
-	};
-
-	/**
-	 * モーダルを表示非表示を切り替える関数です。
-	 */
-	const handleModalToggle = () => {
-		setIsModalOpen(!isModalOpen);
 	};
 
 	/**
@@ -122,23 +87,6 @@ export default function Notes() {
 		);
 		//編集状態を初期化して解除する
 		setIsEditing(INITIAL_EDITING);
-	};
-
-	/**
-	 * 日時の情報を受け取り、表示したいフォーマットにして返却する関数です。（2023-08-03T14:30 → 2023/08/03 14:30）
-	 * @param date string型の日時データです。（2023-08-03T14:30 ）
-	 */
-	const changeDateFormat = (date: string) => {
-		const newDate = new Date(date);
-		const year = newDate.getFullYear();
-		const month = (newDate.getMonth() + 1).toString().padStart(2, "0");
-		const day = newDate.getDate().toString().padStart(2, "0");
-		const hours = newDate.getHours().toString().padStart(2, "0");
-		const minutes = newDate.getMinutes().toString().padStart(2, "0");
-		const dateAndTime = `${year}/${month}/${day} ${hours}:${minutes}`;
-		const dateAndTimeInDB = `${year}-${month}-${day}T${hours}:${minutes}`;
-		const serialDate = newDate.getTime();
-		return { dateAndTime, dateAndTimeInDB, serialDate };
 	};
 
 	/**
@@ -201,20 +149,17 @@ export default function Notes() {
 		await updateDoc(updateRef, updateInformation);
 	};
 
+	/**
+	 * モーダルを表示非表示を切り替える関数です。
+	 */
+	const handleModalToggle = () => {
+		setIsModalOpen(!isModalOpen);
+		console.log("clicked!", isModalOpen);
+	};
+
 	return (
 		<>
-			{isModalOpen ? (
-				<Modal
-					handleModalToggle={handleModalToggle}
-					activeInformation={activeInformation}
-					setActiveInformation={setActiveInformation}
-					informations={informations}
-					setInformations={setInformations}
-					isEditing={isEditing}
-					handleChangeInformation={handleChangeInformation}
-					changeDateFormat={changeDateFormat}
-				/>
-			) : null}
+			{isModalOpen ? <Modal handleModalToggle={handleModalToggle} /> : null}
 			{isEditing.all ? <BgMaskForInput updateSubmit={updateSubmit} /> : null}
 			<Box display={"flex"}>
 				<Stack
@@ -287,7 +232,6 @@ export default function Notes() {
 								activeInformation={activeInformation}
 								handleChangeEditingValue={handleChangeEditingValue}
 								handleClickUpdateElement={handleClickUpdateElement}
-								changeDateFormat={changeDateFormat}
 							/>
 							<NotesParts />
 							<BoardsParts />
