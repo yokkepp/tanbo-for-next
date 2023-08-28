2a786a1192daed2151f0cdb99a480a3395734244
yokkepp <yoseri430@gmail.com>
2023-08-29 08:38:52 +0900

    ✨ Headerに状態を持たせる修正をしました。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { Box, Text, Button } from "@chakra-ui/react";
 import Image from "next/image";
+import { useState } from "react";
 export default function Header() {
 	const menus = [
 		{
@@ -29,6 +30,14 @@ export default function Header() {
 			bgColor: "blueAlpha.900",
 		},
 	];
+	const [page, setPage] = useState("");
+	const handlePage = (path: string) => {
+		setPage(path);
+		//先にwindow.location.pathnameが読み込まれる。
+		//その後レンダリング
+		//useState
+		console.log(path);
+	};
 
 	return (
 		<Box
@@ -45,30 +54,44 @@ export default function Header() {
 			<Box display='flex' alignItems={"center"}>
 				<Image width={100} height={32} src={"/logo.png"} alt={"logo"} />
 				<Box display={"flex"} ml={"50px"}>
-					{menus.map((menu) => {
-						// TODO: 現在のパスを取得し、色を特定する。
-						if ("/note" === menu.link) {
-							return (
-								<Link href={menu.link} key={menu.name}>
-									<Button m={"5px"} bg={menu.bgColor}>
-										{menu.name}
-									</Button>
-								</Link>
-							);
-						} else {
-							return (
-								<Link href={menu.link} key={menu.name}>
-									<Button
-										m={"5px"}
-										bg={"none"}
-										color={menu.bgColor}
-										_hover={{ bg: menu.bgColor, color: "gray.200" }}>
-										{menu.name}
-									</Button>
-								</Link>
-							);
-						}
-					})}
+					<Link href={"/"}>
+						<Button
+							m={"5px"}
+							colorScheme={"gray"}
+							onClick={() => handlePage("/")}
+							variant={page === "/" ? "solid" : "outline"}
+							textColor={"gray.500"}
+							borderColor={"gray.500"}>
+							HOME
+						</Button>
+					</Link>
+					<Link href={"/tasks"}>
+						<Button
+							m={"5px"}
+							colorScheme={"teal"}
+							onClick={() => handlePage("/tasks")}
+							variant={page === "/tasks" ? "solid" : "outline"}>
+							Tasks
+						</Button>
+					</Link>
+					<Link href={"/notes"}>
+						<Button
+							m={"5px"}
+							colorScheme={"orange"}
+							onClick={() => handlePage("/notes")}
+							variant={page === "/notes" ? "solid" : "outline"}>
+							Notes
+						</Button>
+					</Link>
+					<Link href={"/boards"}>
+						<Button
+							m={"5px"}
+							colorScheme={"blue"}
+							onClick={() => handlePage("/boards")}
+							variant={page === "/boards" ? "solid" : "outline"}>
+							Boards
+						</Button>
+					</Link>
 				</Box>
 			</Box>
 			<Button colorScheme={"Black"}>ログアウト</Button>
